50e396b7015756526c54f343f6bdc76c5a74fe4c
Jackie Luc <jackie15910@gmail.com>
2025-02-17 03:43:07 -0500

    Palindrome leetcodes

---
+++ Loops/paldindrome/index.js
@@ -0,0 +1,13 @@
+var isPalindrome = function(x) {
+    if (typeof x === "number" && x < 0) return false;
+        let cleanWords = x.toString().toLowerCase().replace(/[^a-z0-9]/g, '');
+        let reverseWords = cleanWords.split("").reverse().join("");
+        return cleanWords === reverseWords;
+    }
+    
+    console.log(isPalindrome("A man, a plan, a canal: Panama"));
+    console.log(isPalindrome("race a car"));
+    console.log(isPalindrome(" "));
+    console.log(isPalindrome(121));
+    console.log(isPalindrome(122));
+    
--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-var longestConsecutive = function(nums) { // consecutive number counter
-    const sortedNums = nums.sort((a,b) => a - b);
-    if(sortedNums.length === 0){
-        return 0;
-    }
-    console.log(sortedNums);
-    let highestSeq = 1;
-    let currentSeq = 1;
-    for(let i = 0; i < sortedNums.length-1; i++){
-        if(sortedNums[i+1] === sortedNums[i]+1){
-            currentSeq += 1;
-        }
-        else if (sortedNums[i + 1] === sortedNums[i]) {
-            continue;
-        }else{currentSeq = 1};
-        if (currentSeq > highestSeq){
-            highestSeq = currentSeq;
-        }
-    }
-    return highestSeq;
-};
+var isPalindrome = function(x) {
+if (typeof x === "number" && x < 0) return false;
+    let cleanWords = x.toString().toLowerCase().replace(/[^a-z0-9]/g, '');
+    let reverseWords = cleanWords.split("").reverse().join("");
+    return cleanWords === reverseWords;
+}
 
-console.log(longestConsecutive([100,4,200,1,3,2]));
-console.log(longestConsecutive([0,3,7,2,5,8,4,6,0,1]));
-console.log(longestConsecutive([9,1,4,7,3,-1,0,5,8,-1,6]));
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+console.log(isPalindrome("race a car"));
+console.log(isPalindrome(" "));
+console.log(isPalindrome(121));
+console.log(isPalindrome(122));
