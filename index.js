ea05615138d3dad60ba5be0ac689806a36ed83dc
Jackie Luc <jackie15910@gmail.com>
2025-05-13 22:11:59 -0400

    finished max area

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 var maxArea = function(height) {
     let maxArea = 0;
-    for(let i = 0; i < height.length; i++){
-        let j = height.length-1;
-        while(i < j){
-            let smaller = Math.min(i,j);
-            let width = j-i;
-            let area = smaller*width;
-            if(area>maxArea){
-                maxArea = area;
-            }
+    let i = 0;
+    let j = height.length-1;
+    while(i < j){
+        let smaller = Math.min(height[j], height[i]);
+        let width = j-i;
+        let area = smaller*width;
+        if(area>maxArea){
+            maxArea = area;
         }
+        if (height[i] < height[j]) {
+            i++
+        } 
+    else{
+        j--
+    }
     }
     return maxArea;
-};
+}
+
 
 console.log(maxArea([1,8,6,2,5,4,8,3,7]));
